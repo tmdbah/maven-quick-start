cbb0d5bd68d10bc2758dfe2eaa3565d97194e0a1
TJ Bah <31374716+tmdbah@users.noreply.github.com>
2025-12-26 19:17:10 -0500

    Adding new test logic

--- src/main/java/com/tmdbah/training/Application.java
+++ src/main/java/com/tmdbah/training/Application.java
@@ -1,6 +1,19 @@
 package com.tmdbah.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public void greet() {
+        List<String> greetings = new ArrayList<>(); 
+        greetings.add("Hello");
+        greetings.add("Hi");
+
+        for (String greeting : greetings) {
+            System.out.println("Greeting: " + greeting);
+        }
+    }
     
     public Application() {
         System.out.println ("Inside Application");
@@ -10,5 +23,6 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+    app.greet();
     }
 }
